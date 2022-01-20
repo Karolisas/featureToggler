ddcc7a070c609f37c46a86f40fc05b6f99ba82b9
karolisas <karolisas@gmail.com>
2022-01-20 18:58:30 +0200

    UserFeatureTest separated

--- src/test/java/com/example/featureToogler/service/UserFeatureServiceTest.java
+++ src/test/java/com/example/featureToogler/service/UserFeatureServiceTest.java
@@ -19,14 +19,20 @@ import java.util.List;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 //@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 //@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class FeatureServiceTest {
+class UserFeatureServiceTest {
 
     public static final long USER_ID = 1L;
     @Autowired
     FeatureService featureService;
 
     @Autowired
+    UserFeatureService userFeatureService;
+
+    @Autowired
     FeatureRepository featureRepository;
+
+    @Autowired
+    UserFeatureRepository userFeatureRepository;
 
     private Feature feature1;
     private Feature feature2;
@@ -42,72 +48,56 @@ class FeatureServiceTest {
     @AfterEach
     public void deleteTableValues() {
         featureRepository.deleteAll();
+        userFeatureRepository.deleteAll();
     }
 
     @Test
     public void repositoriesInitialized() {
         Assertions.assertNotNull(featureRepository);
+        Assertions.assertNotNull(userFeatureRepository);
+    }
+
+
+    @Test
+    public void enableUserFeature_singleFeatureTest() {
+        Assertions.assertEquals(true, featureService.getFeatures().containsAll(List.of(feature1, feature2)));
+        Assertions.assertEquals(0, userFeatureService.getEnabledFeaturesOnlyForUser(feature1.getId()).size());
+
+        userFeatureService.enableUserFeature(1L, 2L);
+        Assertions.assertEquals(1, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
+        Assertions.assertEquals(0, featureService.getEnabledGlobalFeatures().size());
     }
 
     @Test
-    public void getFeatures() {
-        Assertions.assertArrayEquals(List.of(feature1, feature2).toArray(), featureService.getFeatures().toArray());
-        Assertions.assertEquals(List.of(feature1, feature2), featureService.getFeatures());
-    }
-
-    @Test
-    public void createNewFeature_defaultDisabledTest() {
+    public void getEnabledFeaturesOnlyForUser() {
         Assertions.assertEquals(2, featureService.getFeatures().size());
+        Assertions.assertEquals(0, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
         Assertions.assertEquals(0, featureService.getEnabledGlobalFeatures().size());
-        Assertions.assertEquals(false, featureService.getFeatures().stream()
-                .findFirst()
-                .map(Feature::isEnabledGlobally)
-                .get());
-    }
 
-    @Test
-    public void editFeature_enableGlobalFeature() {
-        Assertions.assertEquals(0, featureService.getEnabledGlobalFeatures().size());
-        Assertions.assertEquals(true, featureService.setEnabledGlobalFeature(feature1.getId(), true).isEnabledGlobally());
-        Assertions.assertEquals(1, featureService.getEnabledGlobalFeatures().size());
-        Assertions.assertEquals(feature1.getId(), featureService.getEnabledGlobalFeatures()
-                .stream()
-                .map(Feature::getId)
-                .filter(id -> id.equals(feature1.getId()))
-                .findFirst()
-                .orElse(null));
-
-        featureService.setEnabledGlobalFeature(feature1.getId(), false);
+        userFeatureService.enableUserFeature(1L, 2L);
+        Assertions.assertEquals(1, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
         Assertions.assertEquals(0, featureService.getEnabledGlobalFeatures().size());
     }
 
     @Test
-    public void editFeature_whichDoesNotExist() {
+    public void getCommonEnabledAndUserFeatures() {
         Assertions.assertEquals(2, featureService.getFeatures().size());
-        Assertions.assertThrows(EntityNotFoundException.class, () -> featureService.setEnabledGlobalFeature(33L, true));
-    }
-
-    @Test
-    public void deleteFeature_Test() {
-        Feature feature = new Feature();
-        featureService.createNewFeature(feature);
-        Assertions.assertEquals(feature, featureService.getFeatures().stream()
-                .filter(f -> f.equals(feature))
-                .findFirst()
-                .orElse(null));
-
-        featureService.deleteFeature(feature.getId());
-        Assertions.assertFalse(featureService.getFeatures().contains(feature));
-    }
-
-    @Test
-    public void getEnabledFeaturesTest() {
-        Assertions.assertEquals(2, featureService.getFeatures().size());
+        Assertions.assertEquals(0, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
         Assertions.assertEquals(0, featureService.getEnabledGlobalFeatures().size());
+        Assertions.assertEquals(0, userFeatureService.getGlobalEnabledAndUserFeatures(USER_ID).size());
 
+
+        userFeatureService.enableUserFeature(USER_ID, feature2.getId());
         featureService.setEnabledGlobalFeature(feature1.getId(), true);
+
+        Assertions.assertEquals(1, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
         Assertions.assertEquals(1, featureService.getEnabledGlobalFeatures().size());
+        Assertions.assertEquals(2, userFeatureService.getGlobalEnabledAndUserFeatures(USER_ID).size());
     }
 
-
+    @Test
+    public void getEnabledUserFeatures_notExist() {
+        Assertions.assertEquals(0, userFeatureService.getEnabledFeaturesOnlyForUser(USER_ID).size());
+        Assertions.assertEquals(0, userFeatureService.getGlobalEnabledAndUserFeatures(USER_ID).size());
+    }
 }
